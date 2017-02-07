ba49356fa7c7fda3a0e6e5df9a900229dbc4588d
gengjf <gengjf0512@163.com>
2017-02-07 16:46:03 +0800

    增加代码需修改处错误提示

--- DingTalkDylib+WIFI/DingTalkDylib/Category/NSBundle+JFUtil/NSBundle+JFUtil.h
+++ DingTalkDylib+WIFI/DingTalkDylib/Category/NSBundle+JFUtil/NSBundle+JFUtil.h
@@ -8,9 +8,9 @@
 
 #import <Foundation/Foundation.h>
 
-//#warning 记得修改自己签名证书的bundleIdentifier，或者使用com.*.*的证书，签名时才不会导致原bundleIdentifier被篡改
+#error 记得修改自己签名证书的bundleIdentifier，或者使用com.*.*的证书，签名时才不会导致原bundleIdentifier被篡改
 // 签名证书的bundleIdentifier
-#define JFBundleIdentifier @"com.99bill.forVendor"
+#define JFBundleIdentifier @"com.*.*"
 // 被注入app自身的bundleIdentifier，此处设置的是钉钉的
 #define JFOrigBundleIdentifier @"com.laiwang.DingTalk"
 
--- DingTalkDylib/DingTalkDylib/Category/NSBundle+JFUtil/NSBundle+JFUtil.h
+++ DingTalkDylib/DingTalkDylib/Category/NSBundle+JFUtil/NSBundle+JFUtil.h
@@ -8,7 +8,7 @@
 
 #import <Foundation/Foundation.h>
 
-#warning 记得修改自己签名证书的bundleIdentifier，或者使用com.*.*的证书，签名时才不会导致原bundleIdentifier被篡改
+#error 记得修改自己签名证书的bundleIdentifier，或者使用com.*.*的证书，签名时才不会导致原bundleIdentifier被篡改
 // 签名证书的bundleIdentifier
 #define JFBundleIdentifier @"com.xx.xx"
 // 被注入app自身的bundleIdentifier，此处设置的是钉钉的
